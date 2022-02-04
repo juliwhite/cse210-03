c0e565264a0531bc0349c9e8bffa291b594f2f76
juliwhite <julietawhite30@gmail.com>
2022-02-03 17:29:08 -0700

    3 files added

---
+++ jumper/game/player.py
@@ -0,0 +1,7 @@
+
+class Player:
+    """
+    This class will create a player that guess a letter to resolve the puzzle. 
+    """
+    def __init__(self) -> None:
+        pass
---
+++ jumper/game/puzzle.py
@@ -0,0 +1,9 @@
+
+import random
+
+class Puzzle:
+    """
+    This class will generate a secret work from a list. 
+    """
+    def __init__(self) -> None:
+        pass
