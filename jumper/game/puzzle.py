c4aca58a0aabdb33f2a7cbb32f1f4dee611872ac
juliwhite <julietawhite30@gmail.com>
2022-02-03 20:49:22 -0700

    add a list words

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -1,8 +1,10 @@
 
 import random
 
+words = ["love", "family", "home", "smile"]
+
 class Puzzle:
-    """
+    """ 
     This class will generate a secret work from a list. 
     """
     def __init__(self) -> None:
